60bccaaad243943b74430fdc6cde42598ce02347
jamily <jamilyfernandes71@gmail.com>
2022-06-07 22:14:23 -0300

    Inplementando o got e post do tema

--- src/app/tema/tema.component.ts
+++ src/app/tema/tema.component.ts
@@ -1,6 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { environment } from 'src/environments/environment.prod';
 import { Router } from '@angular/router';
+import { Tema } from '../model/Tema';
+import { TemaService } from './../service/tema.service';
 
 @Component({
   selector: 'app-tema',
@@ -8,9 +10,13 @@ import { Router } from '@angular/router';
   styleUrls: ['./tema.component.css']
 })
 export class TemaComponent implements OnInit {
-
+  tema: Tema = new Tema() /*instanciando */
+  listaTemas: Tema[]
+  
   constructor(
-    private router: Router
+    private router: Router,
+    private temaService: TemaService /*denpendencia que vamos prescisar, que vem do tema.service */
+
   ) { }
 
   ngOnInit() {
@@ -19,6 +25,22 @@ export class TemaComponent implements OnInit {
       this.router.navigate(['/entrar'])
 
     }
+
+    this.findAllTemas() /*para que toda vez que iniciar a tema mostre todos os temas */
   }
 
+  findAllTemas(){ /*para listar  todos os temas */
+    this.temaService.getAllTema().subscribe((resp: Tema[]) => {
+      this.listaTemas = resp
+    })
+  }
+
+  cadastrar(){
+    this.temaService.postTema(this.tema).subscribe((resp: Tema)=>{
+      this.tema = resp
+      alert("Tema cadastrado")
+      this.findAllTemas() /*para qunado inserir um novo tema, ele mostrar a lista atualizada */
+      this.tema = new Tema() /*para quando cadastrar, o tema não ficar na caixinha */
+    })
+  }
 }
